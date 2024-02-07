c3a2e1d872e12cf6046189a218a10b26ebbfbb80
oras <1941075280@qq.com>
2024-02-07 13:46:07 +0800

    add redisson分布式锁

--- src/main/java/com/oras/usercenter/job/PreCacheJob.java
+++ src/main/java/com/oras/usercenter/job/PreCacheJob.java
@@ -6,6 +6,8 @@ import com.oras.usercenter.mapper.UserMapper;
 import com.oras.usercenter.model.domain.User;
 import com.oras.usercenter.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -30,30 +32,50 @@ public class PreCacheJob {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
 
     //重点用户
     private List<Long> mainUserList = Arrays.asList(1L);
 
     //每天执行，预热推荐用户
-    @Scheduled(cron = "0 6 15 * * *")
+    @Scheduled(cron = "0 50 23 * * *")
     public void doCacheCommendUser(){
 
-        for (Long userId : mainUserList){
+        RLock lock = redissonClient.getLock("teamup:precachejob:docache:lock");
+        try{
+            //  唯一线程可获取锁
+            if( lock.tryLock(0,-1,TimeUnit.MILLISECONDS) ){
+                System.out.println("getLock: " + Thread.currentThread().getId());
+                for (Long userId : mainUserList){
 
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
 
-            String redisKey = String.format("teamup:user:recommend:%s", userId);
-            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    String redisKey = String.format("teamup:user:recommend:%s", userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    //写缓存 30s过期
+                    try {
+                        valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+                    } catch (Exception e) {
+                        log.error("redis set key error ", e );
+                    }
+
+                }
+            }
 
 
-
-            try {
-                valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
-                log.error("redis set key error ", e );
+        }catch (InterruptedException e){
+            System.out.println("unLock: " + Thread.currentThread().getId());
+            log.error("doCacheCommendUser error",e);
+        }finally {
+            //线程只能释放自己的锁
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
             }
         }
+
 
     }
 
